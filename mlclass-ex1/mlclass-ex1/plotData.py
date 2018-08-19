cf6096b67dac3a755296aef304a167c9caf8eb95
kduy <duyvkvn@gmail.com>
2018-08-19 22:46:01 +0200

    Update plotData.py

--- mlclass-ex1/mlclass-ex1/plotData.py
+++ mlclass-ex1/mlclass-ex1/plotData.py
@@ -16,9 +16,5 @@ def plotData(x, y):
     # fig = plt.figure() # open a new figure window
     
 
-    plt.plot(x,y,'rx', markersize=10, label='Training data')
-    plt.xlabel('Population of City in 10,000s')
-    plt.ylabel('Profit in $10,000s')
-    plt.show(block=False) # prevents having to close the graph to move forward with ex1.py
-
+    
 # ============================================================
